a617645d6cc868e32de2b3636878e94f4c9a6e81
Elvis Nyanzui <info9242ngumujobs@gmail.com>
2024-11-05 14:16:35 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = ("What is your name?")
+age = ("How old are you?")
+location = ("Where are you from locationwise")
+
+print(name,age,location)
+
+print("Hello[name],you are[age] years old and live in [location].")
